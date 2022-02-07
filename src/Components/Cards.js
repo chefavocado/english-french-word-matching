afaf07a2ca9c49b721b1391f19fd17c2833da5e7
chefavocado <85806647+chefavocado@users.noreply.github.com>
2022-02-06 21:01:05 -0500

    shuffle

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,33 +1,44 @@
-import React from "react";
+import react from "react";
 import styles from "../Components/styles.css"
 
 function Cards() {
-  const ENGLISH_WORDS = ["Hello", "Cat", "Strawberry", "Boy", "I"]
-  const FRENCH_WORDS = ["Bonjour", "Chat", "Fraise", "Garçon", "Je"]
-
-  function handleClick(e) {
-    e.target.style.backgroundColor = "violet"; //with this line of code, e(the event, a click), targets the elements style and updates its background colour.  
-    e.target.style.color = "white";
-
-    
+  
+  const TRANSLATION_MAP = {
+    "Hello": "Bonjour",
+    "Cat": "Chat",
+    "Strawberry": "Fraise",
+    "Boy": "Garçon",
+    "I": "Je"
   }
+
+  const SHUFFLE_ARRAY = (array) => {
+    array.sort(() => Math.random() - 0.5);
+    return array;
+  }
+
+  const HANDLE_MATCH = () => {
+    if()
+  }
+
+  const ENGLISH_WORDS = SHUFFLE_ARRAY(["Hello", "Cat", "Strawberry", "Boy", "I"]);
+  const FRENCH_WORDS = SHUFFLE_ARRAY(["Bonjour", "Chat", "Fraise", "Garçon", "Je"]);
 
   return (
     <div className="button-container">
       <div className="button-wrapper">
-        <button onClick={handleClick}>{ENGLISH_WORDS[0]}</button>
-        <button onClick={handleClick}>{FRENCH_WORDS[3]}</button>
-        <button onClick={handleClick}>{ENGLISH_WORDS[1]}</button>
-        <button onClick={handleClick}>{FRENCH_WORDS[4]}</button>
-        <button onClick={handleClick}>{ENGLISH_WORDS[2]}</button>
-        <button onClick={handleClick}>{FRENCH_WORDS[1]}</button>
-        <button onClick={handleClick}>{ENGLISH_WORDS[3]}</button>
-        <button onClick={handleClick}>{FRENCH_WORDS[2]}</button>
-        <button onClick={handleClick}>{ENGLISH_WORDS[4]}</button>
-        <button onClick={handleClick}>{FRENCH_WORDS[0]}</button>
+        <button>{ENGLISH_WORDS[0]}</button>
+        <button>{FRENCH_WORDS[0]}</button>
+        <button>{ENGLISH_WORDS[1]}</button>
+        <button>{FRENCH_WORDS[1]}</button>
+        <button>{ENGLISH_WORDS[2]}</button>
+        <button>{FRENCH_WORDS[2]}</button>
+        <button>{ENGLISH_WORDS[3]}</button>
+        <button>{FRENCH_WORDS[3]}</button>
+        <button>{ENGLISH_WORDS[4]}</button>
+        <button>{FRENCH_WORDS[4]}</button>
       </div>
     </div>
   )
 };
 
-export default Cards    
+export default Cards
